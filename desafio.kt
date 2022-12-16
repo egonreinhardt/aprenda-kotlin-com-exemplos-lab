a19f78cb85da1ffc61d7e680427505fa31b032d0
Egon Reinhardt <69218564+egonreinhardt@users.noreply.github.com>
2022-12-16 18:04:41 -0300

    Classes e Funções em funcionamento

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,45 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-class Usuario
+data class Usuario (val username: String, val matricula: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val id: Int, val nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun addUsuario(){
+        for(usuario in inscritos){
+            println("nome: ${usuario.username}")
+        }
+    }
+
+    fun listarInscritos() {
+        inscritos.forEach { u -> println("Nome: ${u.username} - Matrícula: ${u.matricula}") }
+    }
+
+    fun printConteudoEducacional(){
+        for(cont in conteudos){
+            println("id: ${cont.id} - ${cont.nome} - ${cont.duracao}min - (${cont.nivel})")
+        }
+
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val kotlinExperience = listOf(ConteudoEducacional((10..99).random(),"Conteúdo Educacional Exemplo - Módulo 1", 60, Nivel.BASICO),
+        ConteudoEducacional((10..99).random(),"Conteúdo Educacional Exemplo - Módulo 2", 120, Nivel.INTERMEDIARIO))
+
+    val formacaoKotlin = Formacao("Kotlin Experience", kotlinExperience, )
+
+    formacaoKotlin.matricular(Usuario("João da Silva", (1000..9999).random()))
+    formacaoKotlin.listarInscritos()
+    formacaoKotlin.printConteudoEducacional()
+
 }
